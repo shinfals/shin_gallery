6954d237d5c7a69f841d4ee084fa687360e76b1a
shinfals <c011701087@edu.teu.ac.jp>
2019-12-11 11:22:48 +0900

    app.js初期設定

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var favicon = require('serve-favicon');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
