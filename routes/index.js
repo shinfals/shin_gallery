b5eb72b6239b5d1a608ef025fab8bd905a1741c7
shinfals <c011701087@edu.teu.ac.jp>
2019-12-11 13:29:04 +0900

    index.js remake

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var react = require('react');
+var express = require('express');
 var router = react.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
 });
 
 module.exports = router;
