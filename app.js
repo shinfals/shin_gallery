ee518ace3f63e30fa433660e007d488aa55b8fd8
shinfals <c011701087@edu.teu.ac.jp>
2019-12-14 11:55:53 +0900

    add sql query

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var con = mysql.createConnection({
+  host: 'my-site-db',
+  user: 'root',
+  password: 'root',
+  database: 'my-site-db'
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jsx');
@@ -24,6 +31,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//sql query add
+app.use('/',function(req, res, next){
+  var query_str = 'select * from member'
+  con.query(query_str, function(error, results, fields){
+    if (error) throw error;
+    res.render('index',{content:results});
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
