14563becc9299fc947d8c2464789a841a16926cb
shinfals <c011701087@edu.teu.ac.jp>
2019-12-14 11:26:52 +0900

    sql query result add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+var mysql = require('mysql');
 var express = require('express');
 var router = express.Router();
 
+var con = mysql.createConnection({
+  host: 'my-site-db',
+  user: 'root',
+  password: 'root',
+  database: 'my-site-db'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+  //sql query add
+  con.query('select * from member', function(error, results, fields){
+    if (error) throw error;
+    console.log(results);
+    res.render('index', {
+      title: 'Index',
+      content: results
+    });
+  });
 });
 
 module.exports = router;
